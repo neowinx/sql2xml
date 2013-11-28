be6223050744685159e3c2bd163c159cba5c55df
pflores <pdavidflores@gail.com>
2013-11-28 05:38:07 -0300

    Added Some support

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -11,24 +11,29 @@ object Application extends Controller {
 
   def index = Action { implicit request =>
     var xmlBody = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>"
-    xmlBody += "<USERS>"
+    xmlBody += "<payload>"
     DB.withConnection { implicit c =>
-      val consulta = SQL(" SELECT id as toposte, email FROM User ")()
+      val consulta = SQL(" SELECT * FROM User ")()
       println(consulta)
       consulta foreach {
         row => {
-          xmlBody += "<USER>"
+          xmlBody += "<user>"
           val rowMap = row.asMap
           row.metaData.ms map {
             i => {
-              val colAlias = i.column.alias.mkString
-              xmlBody += "<" + colAlias + ">" + rowMap(i.column.qualified) + "</" + colAlias + ">"
+              val colAlias = i.column.alias.mkString.toLowerCase
+              val valor = rowMap(i.column.qualified) match {
+                case Some(x) => x
+                case None => ""
+                case y => y
+              }
+              xmlBody += "<" + colAlias + ">" + valor + "</" + colAlias + ">"
             }
           }
-          xmlBody += "</USER>"
+          xmlBody += "</user>"
         }
       }
-      xmlBody += "</USERS>"
+      xmlBody += "</payload>"
     }
     Ok(scala.xml.XML.loadString(xmlBody))
     //Ok(xmlBody)
--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE User (
     id bigint(20) NOT NULL AUTO_INCREMENT,
-    email varchar(255) NOT NULL,
+    email varchar(255),
     password varchar(255) NOT NULL,
     fullname varchar(255) NOT NULL,
     isAdmin boolean NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE User (
 );
 
 INSERT INTO User (id,email,password,fullname,isAdmin) VALUES (1,'admin@test.com','password','Administrador',true);
-INSERT INTO User (id,email,password,fullname,isAdmin) VALUES (2,'popo@test.com','popo','Popo',true);
+INSERT INTO User (id,email,password,fullname,isAdmin) VALUES (2,null,'popo','Popo',true);
 INSERT INTO User (id,email,password,fullname,isAdmin) VALUES (3,'chichi@test.com','chichi','Chichi',true);
 
 # --- !Downs
